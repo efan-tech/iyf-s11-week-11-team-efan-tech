c981a83773a6917ccfa0e8d6937d3e2feb018439
efan-tech <efanwanjohi@gmail.com>
2026-08-24 21:42:00 +0300

    Update feedback.js

--- backend/models/feedback.js
+++ backend/models/feedback.js
@@ -2,28 +2,34 @@ const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
     name: {
       type: String,
       required: true,
       trim: true,
     },
-
     type: {
       type: String,
       required: true,
-      enum: ['Feedback', 'Idea', 'Suggestion'],
+      enum: ['Feedback', 'Idea', 'Suggestion', 'Bug'],
       default: 'Feedback',
     },
-
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 5,
+    },
     message: {
       type: String,
       required: true,
       trim: true,
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('Feedback', feedbackSchema);
